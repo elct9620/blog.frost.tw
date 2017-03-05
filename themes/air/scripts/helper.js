bb12f53882ceb566a726bca7223be19943fe8a0f
蒼時弦也 <elct9620@frost.tw>
2017-03-05 11:13:18 +0800

    Disable AMP, use Cloudflare support

--- themes/air/scripts/helper.js
+++ themes/air/scripts/helper.js
@@ -1,7 +1,8 @@
 // Template Helper
 
 hexo.extend.helper.register('is_amp', function() {
-    return Boolean(this.page.__amp)
+    return false
+    //return Boolean(this.page.__amp)
 })
 
 hexo.extend.helper.register('amp_url', function(permalink) {
