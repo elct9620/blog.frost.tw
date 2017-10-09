d6583fd6c0a5c5342799870e5f36b53d61aea7c2
蒼時弦也 <elct9620@frost.tw>
2017-10-10 00:57:01 +0800

    Site updated: 2017-10-10 00:57:00

---
+++ js/app.js
@@ -0,0 +1,45 @@
+(function() {
+
+    var $html = document.getElementsByTagName('html')[0]
+
+    var reloadDisqus = function() {
+        if(typeof DISQUS === "undefined") {
+            (function() {
+                var dsq = document.createElement('script');
+                dsq.type = 'text/javascript';
+                dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            }())
+        } else {
+            DISQUS.reset({
+                reload: true,
+                config: function() {
+                    this.page.identifier = document.title
+                    this.page.url = location.href
+                }
+            })
+        }
+    }
+
+    var onPageReload = function() {
+        // Refresh Typekit
+        // try { Typekit.load({async: true}) } catch(e) {}
+
+        // Reload Disqus
+        if(location.pathname != "/") {
+            // Prevent homepage reload Disqus
+            reloadDisqus()
+        }
+    }
+
+    var initTurbolinks = function() {
+        document.addEventListener("turbolinks:load", onPageReload)
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        initTurbolinks()
+    })
+
+}())
+
