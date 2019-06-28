30d55911f2a870a26d4d3521f6e2525a465ff2fd
蒼時弦也 <elct9620@frost.tw>
2019-06-29 03:44:21 +0800

    Improve SEO config

---
+++ themes/air/scripts/description.js
@@ -0,0 +1,19 @@
+/**
+ * Description Helper
+ */
+
+hexo.extend.helper.register('description', function(post, site, is_category, is_tag, __) {
+  var description = []
+
+  if (post.current >= 1) {
+    description.push(__('page', post.current))
+  }
+
+  if(is_category() || is_tag()) {
+    description.push(post.category || post.tag)
+  }
+
+  description.push(post.description || site.description)
+
+  return description.join(' | ')
+})
