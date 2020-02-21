3cbfed6328d0f14192aa810a906a071043aef880
elct9620 <elct9620@users.noreply.github.com>
2020-02-21 10:09:18 +0000

    Publishing Site blog.frost.tw at 38e1f906d43dc3155304332bc0ced8fd73041648 on Fri Feb 21 10:09:18 UTC 2020

---
+++ js/app.min.fd0f63ba3b20398c6e27ff59f1aa04d04afe06105c17ff6f6154ab0d4839bd38.js
@@ -0,0 +1,8 @@
+(function(){var $html=document.getElementsByTagName('html')[0]
+var reloadDisqus=function(){if(typeof DISQUS==="undefined"){(function(){var dsq=document.createElement('script');dsq.type='text/javascript';dsq.async=true;dsq.src='//'+window.disqus_shortname+'.disqus.com/embed.js';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(dsq);}())}else{DISQUS.reset({reload:true,config:function(){this.page.identifier=document.title
+this.page.url=location.href}})}}
+var onPageReload=function(ev){if(location.pathname!="/"){reloadDisqus()}
+if(typeof FB!=="undefined"&&FB!==null){FB.XFBML.parse();}
+if(typeof dataLayer!=="undefined"&&dataLayer!==null){dataLayer.push({'event':'turbolinks:load','virtualUrl':event.data.url});}}
+var initTurbolinks=function(){document.addEventListener("turbolinks:load",onPageReload)}
+document.addEventListener("DOMContentLoaded",function(){initTurbolinks()})}())
