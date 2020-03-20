13e3c5dd6ac64260cb9c5d13161454baa344e361
elct9620 <elct9620@users.noreply.github.com>
2020-03-20 08:51:14 +0000

    Publishing Site blog.frost.tw at cac0a8a0fa6c13e362be8b16e8c094f5dd230217 on Fri Mar 20 08:51:14 UTC 2020

---
+++ js/app.min.3b4fb51ff88af8769c725d45dc3d296b006596a8eb2118a56e7bbfacf4a08d34.js
@@ -0,0 +1,15 @@
+(function(){var $html=document.getElementsByTagName('html')[0]
+var disqusReload=false;var debounce=function(func,delay){var timer=null;return function(){var context=this;var args=arguments;clearTimeout(timer)
+timer=setTimeout(function(){func.apply(context,args)},delay)}}
+var reloadDisqus=debounce(function(){if(location.pathname=="/"){return}
+if(typeof DISQUS==="undefined"){(function(){var dsq=document.createElement('script');dsq.type='text/javascript';dsq.async=true;dsq.src='//revo-skill-frost.disqus.com/embed.js';(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(dsq);}())
+return}
+if(disqusReload){DISQUS.reset({reload:true,config:function(){this.page.identifier=document.title
+this.page.url=location.href}})
+disqusReload=false;}},100)
+var onPageReload=function(ev){if(typeof FB!=="undefined"&&FB!==null){FB.XFBML.parse();}
+if(typeof dataLayer!=="undefined"&&dataLayer!==null){dataLayer.push({'event':'turbolinks:load','virtualUrl':event.data.url});}
+disqusReload=true;}
+var onScroll=function(ev){if(window.scrollY>=100){reloadDisqus()}}
+document.addEventListener("turbolinks:load",onPageReload)
+document.addEventListener("scroll",onScroll)}())
